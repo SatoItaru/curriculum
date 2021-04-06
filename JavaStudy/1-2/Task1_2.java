48ffc7f369cbf0a9473563495ab31ce104abdee9
SatoItaru <itaru1822@gmail.com>
2021-04-06 19:58:05 +0900

    課題提出
    配列

---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,23 @@
+public class Task1_2 {
+
+    public static void main(String[] args) {
+
+        String[] country = new String[3];
+
+        String[] a = {"JAPAN","AMERICA","KOREA"};
+
+        System.out.println(a.length);
+
+        String[] strArray = new String[3];
+
+        String[] b = {"りんご","もも","ぶどう"};
+
+        System.out.println(b[1]);
+
+//[10,20,30,40,50を要素の値とする配列intArrayを作成]
+        int[] intArray = {10,20,30,40,50};
+
+//[上記で作成した配列の1番目と4番目の要素を出力]
+        System.out.println(intArray[1]+intArray[4]);
+    }
+}
