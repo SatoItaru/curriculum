3c0fb6b530257a279252fdf5c6a694782ead46d9
SatoItaru <itaru1822@gmail.com>
2021-04-07 15:35:50 +0900

    課題提出
    Task1_3の作成

---
+++ JavaStudy/1-3/Task1_3.java
@@ -0,0 +1,32 @@
+public class Task1_3 {
+
+    public static void main(String[] args) {
+
+        int plus =  5 + 10;
+
+        int minus =  20 - 7;
+
+//[10と2を乗算した値を代入する変数multiplyを作成]
+        int multiply = 10 * 2;
+
+        int remi = 20 % 6;
+
+//[有楽町を代入する変数townを作成し、線を代入する変数lineを作成]
+        String town = "有楽町";
+
+        String line = "線";
+
+        String train = town + line;
+
+        System.out.println(plus);
+
+        System.out.println(minus);
+
+        System.out.println(multiply);
+
+        System.out.println(remi);
+
+        System.out.println(train);
+
+    }
+}
