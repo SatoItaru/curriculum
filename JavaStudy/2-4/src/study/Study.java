e6459db0c90fb6050f4ea902eaf573f14f1f9540
SatoItaru <itaru1822@gmail.com>
2021-04-23 19:47:51 +0900

    2-4
    課題提出

---
+++ JavaStudy/2-4/src/study/Study.java
@@ -0,0 +1,15 @@
+package study;
+
+import fruits.Fruits;
+
+public class Study {
+	
+	public static void main(String[] args) {
+		
+		Fruits fruits = new Fruits();
+		
+		Fruits.printFruits("レモン", 20);
+		
+	}
+
+}
