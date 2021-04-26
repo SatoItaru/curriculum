a3f77559c81ee5149ae899273d01eb2505f6aaa1
SatoItaru <itaru1822@gmail.com>
2021-04-26 19:24:06 +0900

    課題提出
    2-5

---
+++ JavaStudy/2-5/src/study/Study.java
@@ -0,0 +1,46 @@
+package study;
+
+import java.util.LinkedHashMap ;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+
+public class Study {
+	
+	public static final String SHOP_SHOHIN_00 = "バナナ" ;
+	
+	public static final String SHOP_SHOHIN_01 = "牛乳" ;
+	
+	public static final String SHOP_SHOHIN_02 = "豚肉" ;
+	
+	public static final String SHOP_SHOHIN_03 = "コロッケ" ;
+	
+	public static void main(String args[]) {
+		
+		List<String> shohinList = new LinkedList<String>();
+		
+		shohinList.add("バナナ");
+		
+		shohinList.add("牛乳");
+		
+		shohinList.add("豚肉");
+		
+		shohinList.add("コロッケ");
+		
+		LinkedHashMap<String, Integer> shopMap = new LinkedHashMap<String, Integer>();
+		
+		shopMap.put(shohinList.get(0), 125);
+		
+		shopMap.put(shohinList.get(1), 180);
+		
+		shopMap.put(shohinList.get(2), 350);
+		
+		shopMap.put(shohinList.get(3), 100);
+		
+		for (Entry<String, Integer> s : shopMap.entrySet()) {
+			
+			System.out.println(s +"円になります!");
+		}
+	}
+
+}
